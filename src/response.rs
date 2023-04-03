f31cdfed9e552a5e4bb2f3553aa25deb199134e2
nanderstabel <nanderstabel@gmail.com>
2023-04-03 10:51:27 +0200

    Add comment to SiopResponse

--- src/response.rs
+++ src/response.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+/// Current implementation only supports the `id_token` response type and the cross-device implicit flow.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SiopResponse {
     pub id_token: String,
