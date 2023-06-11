e0969c49b2b25b4969fbcd4d45ef9e23b5d0ab2c
nanderstabel <nander.stabel@impierce.com>
2023-06-11 23:44:15 +0200

    feat: add getters

--- siopv2/src/token/id_token.rs
+++ siopv2/src/token/id_token.rs
@@ -1,6 +1,7 @@
 use super::id_token_builder::IdTokenBuilder;
 use crate::{parse_other, StandardClaimsValues};
 use getset::Getters;
+use getset::Getters;
 use is_empty::IsEmpty;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
@@ -42,14 +43,21 @@ pub struct SubJwk {
 
 /// Set of IANA registered claims by the Internet Engineering Task Force (IETF) in
 /// [RFC 7519](https://tools.ietf.org/html/rfc7519#section-4.1).
-#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, IsEmpty)]
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone, IsEmpty, Getters)]
 pub struct RFC7519Claims {
+    #[getset(get = "pub")]
     pub(crate) iss: Option<String>,
+    #[getset(get = "pub")]
     pub(crate) sub: Option<String>,
+    #[getset(get = "pub")]
     pub(crate) aud: Option<String>,
+    #[getset(get = "pub")]
     pub(crate) exp: Option<i64>,
+    #[getset(get = "pub")]
     pub(crate) nbf: Option<i64>,
+    #[getset(get = "pub")]
     pub(crate) iat: Option<i64>,
+    #[getset(get = "pub")]
     pub(crate) jti: Option<String>,
 }
 
