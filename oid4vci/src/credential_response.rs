460e3ccbab3fb5da0831cc1f488587cb749cf912
nanderstabel <nander.stabel@impierce.com>
2023-08-04 23:33:06 +0200

    test: add unit tests

--- oid4vci/src/credential_response.rs
+++ oid4vci/src/credential_response.rs
@@ -15,6 +15,7 @@ where
     pub c_nonce_expires_in: Option<u64>,
 }
 
+/// Batch Credential Response as described here: https://openid.bitbucket.io/connect/openid-4-verifiable-credential-issuance-1_0.html#name-batch-credential-response.
 #[skip_serializing_none]
 #[derive(Serialize, Debug, PartialEq, Deserialize)]
 pub struct BatchCredentialResponse {
@@ -32,4 +33,93 @@ where
 {
     Deferred { transaction_id: String },
     Immediate(CFC),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::credential_format_profiles::{w3c_verifiable_credentials::jwt_vc_json::JwtVcJson, Credential};
+    use serde_json::json;
+
+    #[test]
+    fn test_credential_response() {
+        let credential_response = CredentialResponse {
+            credential: CredentialResponseType::Deferred {
+                transaction_id: "123".to_string(),
+            },
+            c_nonce: Some("456".to_string()),
+            c_nonce_expires_in: Some(789),
+        };
+        let serialized = serde_json::to_value(&credential_response).unwrap();
+        assert_eq!(
+            serialized,
+            json!({
+                "transaction_id": "123",
+                "c_nonce": "456",
+                "c_nonce_expires_in": 789
+            })
+        );
+        let deserialized: CredentialResponse = serde_json::from_value(serialized).unwrap();
+        assert_eq!(deserialized, credential_response);
+    }
+
+    #[test]
+    fn test_batch_credential_response() {
+        let batch_credential_response = BatchCredentialResponse {
+            credential_responses: vec![
+                CredentialResponseType::Deferred {
+                    transaction_id: "123".to_string(),
+                },
+                CredentialResponseType::Immediate(CredentialFormats::<WithCredential>::JwtVcJson(Credential {
+                    format: JwtVcJson,
+                    credential: json!({
+                        "id": "http://example.edu/credentials/3732",
+                        "type": ["VerifiableCredential", "UniversityDegreeCredential"],
+                        "issuer": "https://example.edu/issuers/14",
+                        "issuanceDate": "2010-01-01T19:23:24Z",
+                        "credentialSubject": {
+                            "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+                            "degree": {
+                                "type": "BachelorDegree",
+                                "name": "Bachelor of Science and Arts"
+                            }
+                        }
+                    }),
+                })),
+            ],
+            c_nonce: Some("456".to_string()),
+            c_nonce_expires_in: Some(789),
+        };
+        let serialized = serde_json::to_value(&batch_credential_response).unwrap();
+        assert_eq!(
+            serialized,
+            json!({
+                "credential_responses": [
+                    {
+                        "transaction_id": "123"
+                    },
+                    {
+                        "format": "jwt_vc_json",
+                        "credential": {
+                            "id": "http://example.edu/credentials/3732",
+                            "type": ["VerifiableCredential", "UniversityDegreeCredential"],
+                            "issuer": "https://example.edu/issuers/14",
+                            "issuanceDate": "2010-01-01T19:23:24Z",
+                            "credentialSubject": {
+                                "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+                                "degree": {
+                                    "type": "BachelorDegree",
+                                    "name": "Bachelor of Science and Arts"
+                                }
+                            }
+                        }
+                    }
+                ],
+                "c_nonce": "456",
+                "c_nonce_expires_in": 789
+            })
+        );
+        let deserialized: BatchCredentialResponse = serde_json::from_value(serialized).unwrap();
+        assert_eq!(deserialized, batch_credential_response);
+    }
 }
