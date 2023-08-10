a46f4ae04fe3e26c5a7706332b99d5ee336a8ccd
nanderstabel <nander.stabel@impierce.com>
2023-08-10 10:56:32 +0200

    feat: add try_descriptor_map

--- oid4vc-manager/src/managers/presentation.rs
+++ oid4vc-manager/src/managers/presentation.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use oid4vp::{
-    evaluate_input, ClaimFormatDesignation, InputDescriptorMappingObject, PathNested, PresentationDefinition,
-    PresentationSubmission,
+    evaluate_input, ClaimFormatDesignation, InputDescriptor, InputDescriptorMappingObject, PathNested,
+    PresentationDefinition, PresentationSubmission,
 };
 
 /// Takes a [`PresentationDefinition`] and a credential and creates a [`PresentationSubmission`] from it if the
@@ -9,34 +9,42 @@ use oid4vp::{
 // TODO: make VP/VC fromat agnostic. In current form only jwt_vp_json + jwt_vc_json are supported.
 pub fn create_presentation_submission(
     presentation_definition: &PresentationDefinition,
-    credential: &serde_json::Value,
+    credentials: Vec<serde_json::Value>,
 ) -> Result<PresentationSubmission> {
     let id = "Submission ID".to_string();
     let definition_id = presentation_definition.id().clone();
     let descriptor_map = presentation_definition
         .input_descriptors()
         .iter()
-        .map(|input_descriptor| {
-            if evaluate_input(input_descriptor, credential) {
-                Ok(InputDescriptorMappingObject {
-                    id: input_descriptor.id().clone(),
-                    format: ClaimFormatDesignation::JwtVpJson,
-                    path: "$".to_string(),
-                    path_nested: Some(PathNested {
-                        id: None,
-                        path: "$.vp.verifiableCredential".to_string(),
-                        format: ClaimFormatDesignation::JwtVcJson,
-                        path_nested: None,
-                    }),
-                })
-            } else {
-                Err(anyhow::anyhow!("Input descriptor evaluation failed"))
-            }
+        .enumerate()
+        .map(|(index, input_descriptor)| {
+            credentials
+                .iter()
+                .find_map(|credential| try_descriptor_map(input_descriptor, index, credential))
+                .unwrap()
         })
-        .collect::<Result<Vec<_>>>()?;
+        .collect::<Vec<_>>();
     Ok(PresentationSubmission {
         id,
         definition_id,
         descriptor_map,
+    })
+}
+
+pub fn try_descriptor_map(
+    input_descriptor: &InputDescriptor,
+    index: usize,
+    credential: &serde_json::Value,
+) -> Option<InputDescriptorMappingObject> {
+    evaluate_input(input_descriptor, credential).then_some(InputDescriptorMappingObject {
+        id: input_descriptor.id().clone(),
+        format: ClaimFormatDesignation::JwtVpJson,
+        path: "$".to_string(),
+        path_nested: Some(PathNested {
+            id: None,
+            path: format!("$.vp.verifiableCredential[{}]", index),
+            format: ClaimFormatDesignation::JwtVcJson,
+            path_nested: None,
+        }),
     })
 }
