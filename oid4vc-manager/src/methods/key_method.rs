7fbd40fe8bcb0ce4efdb55728774ff209ce6896e
nanderstabel <nander.stabel@impierce.com>
2023-10-17 16:40:16 +0800

    fix: use reference

--- oid4vc-manager/src/methods/key_method.rs
+++ oid4vc-manager/src/methods/key_method.rs
@@ -143,7 +143,7 @@ mod tests {
             .unwrap();
 
         let authorization_request =
-            AuthorizationRequest::<Object<SIOPv2>>::from_generic(authorization_request).unwrap();
+            AuthorizationRequest::<Object<SIOPv2>>::from_generic(&authorization_request).unwrap();
 
         // Test whether the provider manager can generate a authorization_response for the authorization_request succesfully.
         let authorization_response = provider_manager
