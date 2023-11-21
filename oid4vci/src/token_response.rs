20667f695a03910efcba2fae60b717d8669b9173
nanderstabel <nander.stabel@impierce.com>
2023-11-21 21:53:56 +0100

    feat: Add Clone trait for TokenResponse

--- oid4vci/src/token_response.rs
+++ oid4vci/src/token_response.rs
@@ -3,7 +3,7 @@ use serde_with::skip_serializing_none;
 
 /// Token Response as described here: https://openid.bitbucket.io/connect/openid-4-verifiable-credential-issuance-1_0.html#name-successful-token-response.
 #[skip_serializing_none]
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct TokenResponse {
     pub access_token: String,
     pub token_type: String,
