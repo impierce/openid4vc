7a12f58f5bac1bce5e6265823c7ec1408c71b020
Nander Stabel <nander.stabel@impierce.com>
2024-02-29 10:25:25 +0100

    refactor: change `ClientMetadata` to an enum that can represent `client_metadata_uri`
    The added benefit is that `SIOPv2` and `OID4VP` specific fields can be
    added to the `ClientMetadata` enum without affecting the `OID4VC` core
    library.

--- oid4vc-core/src/client_metadata.rs
+++ oid4vc-core/src/client_metadata.rs
@@ -1,70 +1,21 @@
-use crate::SubjectSyntaxType;
-use getset::Getters;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 use url::Url;
 
-/// [`ClientMetadata`] is a request parameter used by a [`crate::RelyingParty`] to communicate its capabilities to a [`crate::Provider`].
+/// [`ClientMetadata`] is a request parameter used by a [`crate::RelyingParty`] to communicate its capabilities to a
+/// [`crate::Provider`].
 #[skip_serializing_none]
-#[derive(Getters, Debug, PartialEq, Clone, Default, Deserialize, Serialize)]
-pub struct ClientMetadata {
-    // TODO: Move to siopv2 crate.
-    #[getset(get = "pub")]
-    pub subject_syntax_types_supported: Option<Vec<SubjectSyntaxType>>,
-    // TODO: Move to siopv2 crate.
-    #[getset(get = "pub")]
-    pub id_token_signing_alg_values_supported: Option<Vec<String>>,
-    #[getset(get = "pub")]
-    pub client_name: Option<String>,
-    #[getset(get = "pub")]
-    pub logo_uri: Option<Url>,
-}
-
-impl ClientMetadata {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn with_subject_syntax_types_supported(
-        mut self,
-        subject_syntax_types_supported: Vec<SubjectSyntaxType>,
-    ) -> Self {
-        self.subject_syntax_types_supported = Some(subject_syntax_types_supported);
-        self
-    }
-
-    pub fn with_id_token_signing_alg_values_supported(
-        mut self,
-        id_token_signing_alg_values_supported: Vec<String>,
-    ) -> Self {
-        self.id_token_signing_alg_values_supported = Some(id_token_signing_alg_values_supported);
-        self
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::DidMethod;
-    use std::str::FromStr;
-
-    #[test]
-    fn test_client_metadata() {
-        let client_metadata: ClientMetadata = serde_json::from_value(serde_json::json!(
-            {
-                "subject_syntax_types_supported": [
-                    "did:example",
-                    "urn:ietf:params:oauth:jwk-thumbprint"
-                ]
-            }
-        ))
-        .unwrap();
-        assert_eq!(
-            client_metadata,
-            ClientMetadata::default().with_subject_syntax_types_supported(vec![
-                SubjectSyntaxType::Did(DidMethod::from_str("did:example").unwrap()),
-                SubjectSyntaxType::JwkThumbprint,
-            ])
-        );
-    }
+#[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ClientMetadataEnum<T = ()> {
+    // TODO: Add all fields described in https://www.rfc-editor.org/rfc/rfc7591.html#section-2.
+    ClientMetadata {
+        client_name: Option<String>,
+        logo_uri: Option<Url>,
+        /// As described in [RFC7591](https://www.rfc-editor.org/rfc/rfc7591.html#section-2), the client metadata can be
+        /// expanded with Extensions and profiles.
+        #[serde(flatten)]
+        extension: T,
+    },
+    ClientMetadataUri(String),
 }
