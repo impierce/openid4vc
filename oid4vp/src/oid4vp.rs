431cac70bb488fb1c0aceccb3064c0516464ee99
Nander Stabel <nander.stabel@impierce.com>
2024-03-12 17:55:20 +0100

    chore: add TODO's

--- oid4vp/src/oid4vp.rs
+++ oid4vp/src/oid4vp.rs
@@ -55,6 +55,7 @@ impl Extension for OID4VP {
             .sub(subject_identifier)
             .aud(client_id)
             .nonce(extension_parameters.nonce.to_owned())
+            // TODO: make this configurable.
             .exp((Utc::now() + Duration::minutes(10)).timestamp())
             .iat((Utc::now()).timestamp())
             .verifiable_presentation(user_input.verifiable_presentation.clone())
