0d358f854021183f51fcfcf4fd0013de4d05eaf7
Coplat <chaya777@proton.me>
2025-03-12 12:48:31 +0000

    clippy error fix

--- oid4vci/src/notification_request.rs
+++ oid4vci/src/notification_request.rs
@@ -1,3 +1,5 @@
+use serde_with::skip_serializing_none;
+
 #[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum NotificationEvent {
@@ -5,10 +7,9 @@ pub enum NotificationEvent {
     CredentialFailure,
     CredentialDeleted,
 }
-
+/// Notification Request as described here: https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0-13.html#name-notification-request
 #[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq)]
 #[skip_serializing_none]
-/// Notification Request as described here: https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0-13.html#name-notification-request
 pub struct NotificationRequest {
     pub notification_id: String,
     pub event: NotificationEvent,
