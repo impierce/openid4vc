386010fb1f468c25ceb48740011b6df9b3ddd733
Coplat <chaya777@proton.me>
2025-03-17 16:02:43 +0000

    remove `notification_id` field from `CredentialResponseType`

--- oid4vc-manager/tests/common/memory_storage.rs
+++ oid4vc-manager/tests/common/memory_storage.rs
@@ -141,7 +141,6 @@ impl<CFC: CredentialFormatCollection + DeserializeOwned> Storage<CFC> for Memory
                     .ok()
                 }))
                 .unwrap(),
-                notification_id: None,
             },
             c_nonce: Some(C_NONCE.clone()),
             c_nonce_expires_in: Some(86400),
