16a451f01139c9404d3b1a50c62989de66f68d79
Nander Stabel <nander.stabel@impierce.com>
2025-03-18 14:46:26 +0100

    fix: update `order` field to `Option<Vec<String>>`

--- oid4vci/src/credential_format_profiles/w3c_verifiable_credentials/ldp_vc.rs
+++ oid4vci/src/credential_format_profiles/w3c_verifiable_credentials/ldp_vc.rs
@@ -6,7 +6,7 @@ use super::CredentialSubject;
 
 credential_format!("ldp_vc", LdpVc, {
     credential_definition: CredentialDefinition,
-    order: Option<String>
+    order: Option<Vec<String>>
 });
 
 #[skip_serializing_none]
