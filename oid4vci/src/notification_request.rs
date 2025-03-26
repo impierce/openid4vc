76710eb7d674ddc73f2519d9bb1db37b79432b58
Coplat <chaya777@proton.me>
2025-03-26 15:58:01 +0000

    skip_serializing_none placement

--- oid4vci/src/notification_request.rs
+++ oid4vci/src/notification_request.rs
@@ -8,8 +8,8 @@ pub enum NotificationEvent {
     CredentialDeleted,
 }
 /// Notification Request as described here: https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0-13.html#name-notification-request
-#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Clone)]
 #[skip_serializing_none]
+#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq, Clone)]
 pub struct NotificationRequest {
     pub notification_id: String,
     pub event: NotificationEvent,
