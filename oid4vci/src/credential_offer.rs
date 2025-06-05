651e890dd53145bacc79bac1835cfe466900d1dd
Coplat <chaya777@proton.me>
2025-06-05 13:41:50 +0100

    typo

--- oid4vci/src/credential_offer.rs
+++ oid4vci/src/credential_offer.rs
@@ -78,7 +78,7 @@ impl std::fmt::Display for CredentialOffer {
         match self {
             CredentialOffer::CredentialOfferUri(uri) => {
                 let mut url = Url::parse("openid-credential-offer://").map_err(|_| std::fmt::Error)?;
-                url.query_pairs_mut().append_pair("credential_offer_uri", &uri.as_ref());
+                url.query_pairs_mut().append_pair("credential_offer_uri", uri.as_ref());
                 write!(f, "{}", url)
             }
             CredentialOffer::CredentialOffer(offer) => {
