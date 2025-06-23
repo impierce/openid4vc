1d34301bfaa57abf4fc3f6acc19dad4d330c5c86
Coplat <chaya777@proton.me>
2025-06-23 12:53:10 +0200

    claim_sets logic

--- oid4vp/src/dcql_evaluation.rs
+++ oid4vp/src/dcql_evaluation.rs
@@ -1,4 +1,4 @@
-use crate::dcql::dcql_query::{ClaimPath, ClaimPathElement, ClaimQuery, ClaimValue, ClaimValues};
+use crate::dcql::dcql_query::{ClaimPath, ClaimPathElement, ClaimQuery, ClaimValue, ClaimValues, CredentialQuery};
 use serde_json::Value;
 
 impl ClaimPath {
@@ -64,4 +64,34 @@ pub fn evaluate_single_claim_query(claim_query: &ClaimQuery, credential_json: &V
         }
     }
     true
+}
+
+/// Processing with claims_sets as described in OID4VP - draft 28 Section 6.4.1 Selecting Claims:
+/// https://openid.net/specs/openid-4-verifiable-presentations-1_0.html#name-selecting-claims-and-credentials
+
+pub fn evaluate_credential_query(credential_query: &CredentialQuery, credential_json: &Value) -> bool {
+    //If claims is absent, the Verifier is requesting no claims that are selectively disclosable;
+    // the Wallet MUST return only the claims that are mandatory to present (e.g., SD-JWT and Key Binding JWT for a Credential of format IETF SD-JWT VC).
+    if credential_query.claims.is_empty() {
+        return true;
+    }
+    // If claims is present, but claim_sets is absent, the Verifier requests all claims listed in claims.
+    match &credential_query.claim_sets {
+        None => credential_query
+            .claims
+            .iter()
+            .all(|claim| evaluate_single_claim_query(claim, credential_json)),
+
+        // If both claims and claim_sets are present, the Verifier requests one combination of the claims listed in claim_sets. The order of the options conveyed in the claim_sets array expresses the Verifier's preference for what is returned;
+        // the Wallet SHOULD return the first option that it can satisfy. If the Wallet cannot satisfy any of the options, it MUST NOT return any claims.
+        Some(claim_sets) => claim_sets.iter().any(|claim_set| {
+            claim_set.iter().all(|claim_id| {
+                credential_query
+                    .claims
+                    .iter()
+                    .find(|claim| claim.id.as_ref() == Some(claim_id))
+                    .map_or(false, |claim| evaluate_single_claim_query(claim, credential_json))
+            })
+        }),
+    }
 }
