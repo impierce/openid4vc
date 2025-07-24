e3a9e561b1452bbfc0d38e3780e01d5714c7472c
Coplat <chaya777@proton.me>
2025-07-24 15:44:47 +0100

    lint

--- oid4vc-core/src/utils/predicates.rs
+++ oid4vc-core/src/utils/predicates.rs
@@ -1,4 +1,4 @@
-/// Generic predicate function that validates that a vector is not empty. For use in our nutype contexts.
+/// Generic predicate function that validates that a vector is not empty. For use in our nutype structs.
 pub fn not_empty<T>(vec: &[T]) -> bool {
     !vec.is_empty()
 }
