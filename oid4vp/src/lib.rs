8c6312b3db07cbf640e5853e224d96d1e78fa7e1
Coplat <chaya777@proton.me>
2025-08-04 16:15:05 +0200

    remove oid4vp_params

--- oid4vp/src/lib.rs
+++ oid4vp/src/lib.rs
@@ -2,7 +2,6 @@ pub mod authorization_request;
 pub mod dcql;
 pub mod dcql_evaluation;
 pub mod oid4vp;
-pub mod oid4vp_params;
 pub mod token;
 
-pub use {oid4vp_params::Oid4vpParams, token::vp_token::VpToken};
+pub use token::vp_token::VpToken;
