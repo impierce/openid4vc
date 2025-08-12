a445ea4dfb38a34bcec17cc6fe30d566f88b5cff
Coplat <chaya777@proton.me>
2025-08-12 14:32:15 +0100

    outdated link

--- oid4vp/src/authorization_request.rs
+++ oid4vp/src/authorization_request.rs
@@ -20,7 +20,7 @@ pub struct ClientId {
 }
 
 /// The Client ID Scheme enables the use of different mechanisms to obtain and validate the Verifier's metadata. As
-/// described here: https://openid.net/specs/openid-4-verifiable-presentations-1_0-20.html#name-verifier-metadata-managemen
+/// described here: https://openid.net/specs/openid-4-verifiable-presentations-1_0-28.html#name-client-identifier-prefix-an
 #[derive(Debug, PartialEq, Clone)]
 pub enum ClientIdPrefix {
     PreRegistered,
