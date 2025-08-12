be518d16edc6dfe820db93a88153c19732cb7eec
Coplat <chaya777@proton.me>
2025-08-12 17:03:28 +0100

    rename

--- oid4vp/src/token/vp_token_builder.rs
+++ oid4vp/src/token/vp_token_builder.rs
@@ -246,7 +246,7 @@ mod tests {
     }
 
     #[test]
-    fn test_vp_token_builder_nothing_is_required() {
+    fn test_vp_token_builder_credential_sets_not_required() {
         let dcql_query_json = json!({
             "credentials": [
                 {
@@ -300,7 +300,7 @@ mod tests {
     }
 
     #[test]
-    fn test_vp_token_builder_nothing_is_required_but_one_is_there() {
+    fn test_vp_token_builder_empty_presentations_with_credential_sets() {
         let dcql_query_json = json!({
             "credentials": [
                 {
