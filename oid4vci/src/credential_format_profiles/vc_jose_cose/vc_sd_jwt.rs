dc2c38846dcee5da35ef8592f647179de12a8e56
Nander Stabel <nander.stabel@impierce.com>
2026-02-24 10:41:35 +0100

    feat: update Credential Format extension point for `vc+sd-jwt` (#118)

--- oid4vci/src/credential_format_profiles/vc_jose_cose/vc_sd_jwt.rs
+++ oid4vci/src/credential_format_profiles/vc_jose_cose/vc_sd_jwt.rs
@@ -1,3 +1,14 @@
 use crate::credential_format;
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
-credential_format!("vc+sd-jwt", VcSdJwt, {});
+credential_format!("vc+sd-jwt", VcSdJwt, {
+    credential_definition: CredentialDefinition
+});
+
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+pub struct CredentialDefinition {
+    #[serde(rename = "type")]
+    pub type_: Vec<String>,
+}
