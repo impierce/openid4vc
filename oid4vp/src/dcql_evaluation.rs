d0deb2552ddbebc8e09c4c690675cbb0bc47b3f1
Coplat <chaya777@proton.me>
2026-06-01 17:36:42 +0200

    feat: vct_values check during dcql_evaluation

--- oid4vp/src/dcql_evaluation.rs
+++ oid4vp/src/dcql_evaluation.rs
@@ -118,10 +118,16 @@ pub fn evaluate_credential_query(
                     return false;
                 }
             }
-            MetaTypes::SdJwtMeta {
-                vct_values: _vct_values,
-            } => {
-                // TODO: Implement SD-JWT `vct` checking
+            MetaTypes::SdJwtMeta { vct_values } => {
+                if let Some(vct_string) = decoded_presentation.get("vct").and_then(|v| v.as_str()) {
+                    let type_match = vct_values.iter().any(|required_vct| required_vct == vct_string);
+
+                    if !type_match {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
             }
             MetaTypes::MsoMdocMeta {
                 doctype_value: _doctype_value,
@@ -350,6 +356,77 @@ mod tests {
             dcql_query,
             &DecodedPresentations::try_new(vec![testing_credential.get("vc").unwrap().as_object().unwrap().clone()])
                 .unwrap()
+        ));
+    }
+
+    #[test]
+    fn test_dcql_credential_query_with_compatible_vct_values() {
+        let testing_credential = json!({
+            "vct": "https://credentials.example.com/identity_credential",
+            "given_name": "Ferris",
+            "family_name": "Crabman",
+            "email": "ferris.crabman@crabmail.com",
+            "birthdate": "1985-05-21"
+        });
+        let dcql_request: DcqlRequest = serde_json::from_value(serde_json::json!({
+            "credentials": [
+                {
+                    "id": "login",
+                    "format": "dc+sd-jwt",
+                    "meta": {
+                        "vct_values": ["https://credentials.example.com/identity_credential"]
+                    },
+                    "claims": [{ "path": ["email"] }]
+                }
+            ]
+        }))
+        .unwrap();
+        let dcql_query = &dcql_request.credentials[0];
+
+        let claims_context = ClaimsContext {
+            claim_sets: &dcql_query.claim_sets,
+        };
+        validate_claims(&dcql_query.claims.as_deref().unwrap_or(&[]), &claims_context).unwrap();
+
+        assert!(evaluate_credential_query(
+            dcql_query,
+            &DecodedPresentations::try_new(vec![testing_credential.as_object().unwrap().clone()]).unwrap()
+        ));
+    }
+
+    #[test]
+    fn test_dcql_credential_query_with_incompatible_vct_values() {
+        let testing_credential = json!({
+            "vct": "https://credentials.example.com/identity_credential",
+            "given_name": "Ferris",
+            "family_name": "Crabman",
+            "email": "ferris.crabman@crabmail.com",
+            "birthdate": "1985-05-21"
+        });
+        let dcql_request: DcqlRequest = serde_json::from_value(serde_json::json!({
+            "credentials": [
+                {
+                    "id": "login",
+                    "format": "dc+sd-jwt",
+                    "meta": {
+                        "vct_values": ["https://credentials.example.com/verified_email"]
+                    },
+                    "claims": [{ "path": ["email"] }]
+                }
+            ]
+        }))
+        .unwrap();
+        let dcql_query = &dcql_request.credentials[0];
+
+        let claims_context = ClaimsContext {
+            claim_sets: &dcql_query.claim_sets,
+        };
+        validate_claims(&dcql_query.claims.as_deref().unwrap_or(&[]), &claims_context).unwrap();
+
+        // Assert `false` because the credential vct does not match the required vct_values in the query.
+        assert!(!evaluate_credential_query(
+            dcql_query,
+            &DecodedPresentations::try_new(vec![testing_credential.as_object().unwrap().clone()]).unwrap()
         ));
     }
 
